7d940cc7f64b033a450f89dd6cafc9c1a3dabb3b
VasilesBalabanis <964241@swansea.ac.uk>
2024-06-20 17:04:49 +0100

    Update SimulatedAnnealing.py

--- SimulatedAnnealing.py
+++ SimulatedAnnealing.py
@@ -1,3 +1,13 @@
+'''
+This file is part of the project MEGSubnetworks. 
+All code in MEGSubnetworks is free: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License on this link. 
+You should have received a copy of the GNU General Public License along with MEGSubnetworks. If not, see https://www.gnu.org/licenses/.
+'''
+
+
 import numpy as np
 import random
 import math
