9bafe378bdbe6608eb9a8ed58887b7ad98cf47a9
VasilesBalabanis <964241@swansea.ac.uk>
2024-06-20 17:06:20 +0100

    Update SimulatedAnnealing.py

--- SimulatedAnnealing.py
+++ SimulatedAnnealing.py
@@ -1,4 +1,5 @@
 '''
+LICENSE:
 This file is part of the project MEGSubnetworks. 
 All code in MEGSubnetworks is free: you can redistribute it and/or modify it under the terms of the GNU General Public License 
 as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
